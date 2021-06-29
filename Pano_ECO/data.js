92da2b3bab3cde66b5e2a9d7b73de75751307dab
alisavonpostel <67705877+alisavonpostel@users.noreply.github.com>
2021-06-29 17:29:36 +0200

    Rundgang

--- Pano_ECO/data.js
+++ Pano_ECO/data.js
@@ -24,23 +24,36 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.02465711497403511,
-        "pitch": 0.5303989664846629,
-        "fov": 1.610976895625884
+        "yaw": -0.026624206055494426,
+        "pitch": 0.470556931471112,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -0.307471464106051,
-          "pitch": 1.1559210165180431,
+          "yaw": 0.00014147390519170244,
+          "pitch": 1.1854237022483929,
           "rotation": 0,
-          "target": "1-straenansicht"
+          "target": "1-auenraum"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.6700356853215439,
+          "pitch": -0.16960561991362155,
+          "title": "Stadtklima",
+          "text": "Bis 2050 könnte Stuttgart so heiß werden, wie Rom heute. Das führt zu vermehrten Hitzetoten und längeren Hitzewellen, die besonders in stark versiegelten Zonen zu Wärmeinseln führen. Das Ziel dieses Projektes ist es das Außenraumklima aufzubessern. Um dies zu erreichen gilt grundsätzlich möglichst viel Grün anzusiedeln oder bestehen zu lassen. Durch die hier angewandten Maßnahmen entsteht ein Cool-Spot im Stadtzentrum, der mit weiteren Cool-Spots in der Nähe verknüpft werden kann, sodass der Effekt verstärkt wird.&nbsp;&nbsp; <br>"
+        },
+        {
+          "yaw": -0.9412884287532428,
+          "pitch": -0.13653414270852338,
+          "title": "Projekteinführung<br>",
+          "text": "Das Projekt PARKhaus beschäftigt sich mit der Umnutzung des Züblin Parkhauses an der B14 in Stuttgart. Auf dem Grundstück soll ein Gebäude entstehen, das die Umgebung klimatisch aufwertet. Da ein Neubau auch mit dem Fällen der bestehenden Bäume verbunden wäre, die aber zur Kühlung des Außenraums beitragen, liegt es nahe das bestehende Gebäude mit seinen vielen Potentialen umzurüsten. Gleichzeitig soll eine Verbindung zwischen den zwei Stadtteilen Leonardsviertel und Bohnenviertel geschaffen werden, zwei sehr unterschiedliche Viertel, die am Grundstück aufeinander treffen. Dies wird erreicht, in dem das Konzept des schon bestehenden Kultur Kiosks im Parkhaus übernommen und erweitert wird, sodass Wohnen dann zwischen Gemeinschafts- und Kulturzentrum stattfindet. <br>"
+        }
+      ]
     },
     {
-      "id": "1-straenansicht",
-      "name": "Straßenansicht",
+      "id": "1-auenraum",
+      "name": "Außenraum",
       "levels": [
         {
           "tileSize": 256,
@@ -62,19 +75,26 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.3402688250760093,
-        "pitch": 0.04475203210240508,
+        "yaw": 0.3567643611435436,
+        "pitch": -0.07728954429959423,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -0.3922020831872306,
-          "pitch": -0.0785598084029715,
+          "yaw": -0.37827859079957094,
+          "pitch": 0.06194180990043208,
           "rotation": 0,
           "target": "2-atrium"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 1.2052180421976235,
+          "pitch": -0.5668157046821172,
+          "title": "Bestandsgebäude",
+          "text": "Das bestehende Parkhaus zeichnet sich besonders durch seine Erschließung aus. Im Gegensatz zu den meisten Parkhäusern, gibt es keine zentrale Auffahrt. Stattdessen besteht die Sturktur aus zwei ineinander geschobenen spiralförmigen Rampen, die zusammen eine Doppelhelix erzeugen. Diese endet auf einem großflächigen Dach, das schon heute sowohl für Urban Garding, als auch als Veranstaltungsort genutzt wird.<br>"
+        }
+      ]
     },
     {
       "id": "2-atrium",
@@ -100,23 +120,30 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.1445701743846204,
-        "pitch": -0.04027682889222106,
+        "yaw": -0.04436624291057534,
+        "pitch": 0.03410213665845063,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 1.7733924818046898,
-          "pitch": -0.35226251177984125,
+          "yaw": 1.8585319330667351,
+          "pitch": 0.20465622315500553,
           "rotation": 0,
-          "target": "3-rampe"
+          "target": "3-zwischenraum-rampe"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.7531087839334827,
+          "pitch": -0.4913173342608115,
+          "title": "Neues Erschließungskonzept<br>",
+          "text": "Ein Problem des Bestands ist die große Gebäudetiefe, die für eine Umnutzung die Frage nach ausreichender Belichtung stellt. Verschiedene Lichtstudien haben gezeigt, dass drei Atrien in der Mitte für eine ausreichende Belichtung sorgen, auch im Erdgeschoss, wo private und öffentliche Eingänge situiert sind. Die Atrien werden dann neben den Rampen als schnelle Erschließungskerne genutzt, wo Austausch zwischen den unterschiedlichen Nutzungen stattfindet. Durch den Verschnitt der beiden spiralförmigen Rampen, liegen immer private und öffentliche Nutzungen in einer Ebene, was das Gemeinschaftsgefühl und die Kommunkation fördert.<br>"
+        }
+      ]
     },
     {
-      "id": "3-rampe",
-      "name": "Rampe",
+      "id": "3-zwischenraum-rampe",
+      "name": "Zwischenraum Rampe",
       "levels": [
         {
           "tileSize": 256,
@@ -138,23 +165,30 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.7295424758287901,
-        "pitch": 0.049714221396023106,
+        "yaw": 0.2005690189513345,
+        "pitch": -0.06592343484377139,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 0.5895262238715091,
-          "pitch": -0.3608843816592753,
+          "yaw": -0.8377672666539091,
+          "pitch": 0.04042081782566598,
           "rotation": 0,
-          "target": "4-innenraumperspektive"
+          "target": "4-innenraum"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 1.0458847272702911,
+          "pitch": -0.5605268321104635,
+          "title": "Thermisches Konzept und Versorgungskreisläufe<br>",
+          "text": "<div>Zwischen Außenraum und Innenraum bilden die Rampen und Atrien eine thermische Pufferzone, die sowohl im Winter, als auch im Sommer hilft das Innenraumklima komfortabel zu machen. Um ein nachhaltiges Gebäude zu entwickeln, muss auch das Versorgungssystem bedacht werden. Ziel ist es mit Photovoltaik und Biogasanlage sowohl die Wärme- als auch die Stromversorgung intern zu decken. Eine Wasseraufbereitung über das Gründach hilft Grau- und Regenwasser zu filtern und somit den insgesamten Wasserverbrauch zu verringen.</div><div> Gleichzeitig dient dieser Zwischenraum auf den Rampen als Ort der Gemeinschaft. Auf der \"internen\" Rampe können Familien gemeinsam kochen und die Kinder spielen. Auf der \"externen\" Rampe wird das Konzept des kultur Kiosks weitergeführt: Eine Ausstellung die vom Ergeschoss ins Obergeschoss führt, fördert die kulturelle Vielfalt und zieht externe NutzerInnen an. Dies ermöglicht einen intensiven Austausch, das Parkhaus wird zum Quatiersmittelpunkt.</div>"
+        }
+      ]
     },
     {
-      "id": "4-innenraumperspektive",
-      "name": "Innenraumperspektive",
+      "id": "4-innenraum",
+      "name": "Innenraum",
       "levels": [
         {
           "tileSize": 256,
@@ -176,98 +210,119 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.6314267435345542,
-          "pitch": -0.32494719606691547,
-          "rotation": 0,
-          "target": "5-dachaufbau-innen"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-dachaufbau-innen",
-      "name": "Dachaufbau Innen",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.012341356349907073,
-        "pitch": 0.06712804815362716,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.3246568908984688,
-          "pitch": -0.016328513855736304,
-          "rotation": 0,
-          "target": "6-dach-auen"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-dach-auen",
-      "name": "Dach Außen",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.1304654605061657,
         "pitch": 0,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 1.8355249058020258,
-          "pitch": 0.3093349518081059,
+          "yaw": 0.9253367562971881,
+          "pitch": 0.2435315326737406,
+          "rotation": 0,
+          "target": "5-inneres-obergeschoss"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8151927943875172,
+          "pitch": -0.5153845874056273,
+          "title": "Nutzungsüberschneidungen<br>",
+          "text": "Im schematischen Grundriss erkennt man, wie die beiden Rampen und ihre zugeordnete Nutzung auf einer Ebene zusammenkommen. Die Einsätze für Wohnungen und öffentliche Nutzungen, wie Büros, Geschäfte, Werkstätten oder Ateliers, sind aus Holz gefertigt. Sie ordnen sich jeweils an den Außenraum oder die Atrien an, um ausreichende Lichtzufuhr zu ermöglichen und die Größe und Höhe der Einbauten in der Wahrnehmung zu vergrößern. <br>"
+        }
+      ]
+    },
+    {
+      "id": "5-inneres-obergeschoss",
+      "name": "Inneres Obergeschoss",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2956233905247627,
+          "pitch": 0.24205184754372944,
+          "rotation": 0,
+          "target": "6-dachterrasse"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8141472760205968,
+          "pitch": -0.5147056689478351,
+          "title": "Obergeschoss als Bindeglied<br>",
+          "text": "Wie schon im Erdgeschoss, kommen im Obergeschoss alle unterschiedlichen Nutzungen und deren NutzerInnen zusammen. Hier kann gearbeitet und gespielt werden, oder es können Veranstaltungen stattfinden. Der große sonnendurchflutete Bereich ist durch ein Holztragwerk mit Verglasung vom Außenraum getrennt. Als Bindeglied steht der Raum in unmittelbaren Bezug zur Dachterrasse und den Atrien."
+        }
+      ]
+    },
+    {
+      "id": "6-dachterrasse",
+      "name": "Dachterrasse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.82661979281713,
+          "pitch": 0.10694430408127076,
           "rotation": 0,
           "target": "0-vogelperspektive"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.81607298669695,
+          "pitch": -0.5179968986658139,
+          "title": "Dachterrasse als Ort der Gemeinschaft<br>",
+          "text": "Auf der Dachterrasse kommen natur- und menschbezogene Aspekte des Projekts zusammen. Die Begrünung funktioniert einersteits als Kühlung des Außenraums, als Filtersystem für Grau- und Regenwasser, schafft Biodiversität, bietet aber gleichzeitig Fläche für Urban Gardeing, ein Konzept, das Menschen zusammenbringt und mit der Natur verbindet. Weitere Flächen können für Sport und Veranstaltungen, wie Tanzabende oder Freilichtkino genutzt werden. <br>"
+        }
+      ]
     }
   ],
-  "name": "PARK haus",
+  "name": "PARKhaus",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
